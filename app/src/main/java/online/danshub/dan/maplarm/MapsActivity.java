3dc98393f1fcfedbe80fcf85813812bee79ee1a0
DanTheMinotaur <x15004091@student.ncirl.ie>
2018-11-21 23:35:51 +0000

    Map Marker Click

--- app/src/main/java/online/danshub/dan/maplarm/MapsActivity.java
+++ app/src/main/java/online/danshub/dan/maplarm/MapsActivity.java
@@ -2,27 +2,35 @@ package online.danshub.dan.maplarm;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private final ArrayList<Marker> markers = new ArrayList<Marker>();
+    private Marker currentMarker = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
 
@@ -39,11 +47,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        Log.v("Location", "Marker Create Array");
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                if (currentMarker != null) {
+                    currentMarker.remove();
+                    currentMarker = null;
+                }
 
+                //Log.v("Maps", "Lat: " + latLng.latitude + " | Long: " + latLng.longitude + " Markers = " + markers.size());
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                //markers.add(mMap.addMarker(new MarkerOptions().position(latLng)));
+                currentMarker = mMap.addMarker(new MarkerOptions().position(latLng));
+                Log.v("Marker", "Size: " + markers.size());
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+            }
+        });
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                currentMarker.remove();
+            }
+        });
     }
 }
