2bb171498cbb0c42419d885636e6a87015581a6e
DanTheMinotaur <x15004091@student.ncirl.ie>
2018-11-24 16:26:56 +0000

    Splash Screen.

---
+++ app/src/main/java/online/danshub/dan/maplarm/SplashActivity.java
@@ -0,0 +1,19 @@
+package online.danshub.dan.maplarm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.v("SPLASH", "I'm in the splash screen");
+        Toast.makeText(getApplicationContext(), "I'm in the SPLASH SCREEN!", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(SplashActivity.this, MapsActivity.class));
+        finish();
+    }
+}
